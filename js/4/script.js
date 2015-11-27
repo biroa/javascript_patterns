d8f639a29881786ba1156921c23e334685c86f80
biroa <adam.biro@gmail.com>
2015-11-27 21:18:56 +0000

    Module Reveal Pattern

--- js/4/script.js
+++ js/4/script.js
@@ -1,17 +1,20 @@
 /**
  * Created by biroa on 12/25/2015.
  *
- * Module Reveal Pattern init
+ * Module Reveal Pattern
+ *
+ * add underscore tp private variables and functions
+ * expose in return{public:functions}
  */
 
 var chatModule = (function(){
-
-    var leadself = 'Me: ',
-        leadcomputer = "PC: ",
-        aSaid = ["This is a Cyber Chat"],
-        msgYes = "Yes, that's a great idea.",
-        msgNo = "No, that must be a mistake.",
-        aSassyStuff = [
+    //init private functions and variables
+    var _leadself = 'Me: ',
+        _leadcomputer = "PC: ",
+        _aSaid = ["This is a Cyber Chat"],
+        _msgYes = "Yes, that's a great idea.",
+        _msgNo = "No, that must be a mistake.",
+        _aSassyStuff = [
         "Like mold on books, grow myths on history.",
         "She moved like a poem and smiled like a sphinx.",
         "As long as we don’t die, this is gonna be one hell of a story.",
@@ -19,32 +22,39 @@ var chatModule = (function(){
         "You’ve got about as much charm as a dead slug."
     ];
 
-    function echo(msg) {
-        aSaid.push("<div>" + msg + "</div>");
+    function _echo(msg) {
+        _aSaid.push("<div>" + msg + "</div>");
 
-        var aSaidLength = aSaid.length;
-        start = Math.max(aSaid.length - 6, 0),
+        var aSaidLength = _aSaid.length;
+        start = Math.max(_aSaid.length - 6, 0),
             out = "";
 
         for (var i = start; i < aSaidLength; i++) {
-            out += aSaid[i];
+            out += _aSaid[i];
         }
         $('.advert').html(out);
         $('#talk span').text(msg);
     }
 
-    return {
-        talk: function (msg) {
-            echo(leadself + msg);
-        },
-        replayYesNo: function () {
-            var msg = Math.random() > .5 ? msgYes : msgNo;
-            echo(leadcomputer + msg);
-        },
-        saySassyStuff: function () {
-            var msg = aSassyStuff[Math.floor(Math.random() * aSassyStuff.length)];
-            echo(leadcomputer + msg);
-        }
+    function talk(msg) {
+        _echo(_leadself + msg);
+    }
+
+    function replayYesNo() {
+        var msg = Math.random() > .5 ? _msgYes : _msgNo;
+        _echo(_leadcomputer + msg);
+    }
+
+    function saySassyStuff() {
+        var msg = _aSassyStuff[Math.floor(Math.random() * _aSassyStuff.length)];
+        _echo(_leadcomputer + msg);
+    }
+
+
+    return {//expose public functions
+        talk:talk,
+        replayYesNo:replayYesNo,
+        saySassyStuff:saySassyStuff
     }
 })();
 
